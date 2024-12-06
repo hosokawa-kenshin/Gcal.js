94b6b4c64de31d368b27dc8375348e8b98ab73d6
hosokawa-kenshin <pnl84lku@s.okayama-u.ac.jp>
2024-12-06 16:26:53 +0900

    Add validation of jump command

--- src/commands/jump.js
+++ src/commands/jump.js
@@ -4,9 +4,22 @@ import { convertToDateTime } from "../utils/dateUtils.js";
 export function jumpCommand(screen, events, args){
   const leftTable = screen.children.find(child => child.options.label === 'Upcoming Events');
   const logTable = screen.children.find(child => child.options.label === 'Gcal.js Log');
+  if (args.length === 0) {
+    leftTable.select(searchIndex(new Date, events));
+    leftTable.scrollTo(leftTable.selected + leftTable.height - 3);
+    logTable.log(`Jumped to today`);;
+    screen.render();
+    return;
+  }
   const date = convertToDateTime(args[0]);
+  if (isNaN(date.getTime())) {
+    logTable.log(`Invalid date: ${args[0]}`);
+    screen.render();
+    return;
+  }
   leftTable.select(searchIndex(date, events));
   leftTable.scrollTo(leftTable.selected + leftTable.height - 3);
   logTable.log(`Jumped to ${date}`);
   screen.render();
+  return;
 }
